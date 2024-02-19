f6c4dc3969a1a9b3d628420769bc7e4409b6f328
Sanjeev <sk3616593@gmail.com>
2024-02-19 23:19:14 +0530

    frontend done

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
-
+import './normal.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <aside className='sidemenu'>
+        <div className='side-menu-button'>
+
+          <span>+</span>
+          New chat
+        </div>
+      </aside>
+      <section className='chatbox'>
+        <div className='chat-log'>
+          <div className='chat-message'>
+            <div className='chat-message-center'>
+              <div className='avatar'>
+        
+              </div>
+              <div className='message'>
+                Hello Sanjeev
+              </div>
+            </div>
+          </div>
+          <div className='chat-message chatgpt'>
+            <div className='chat-message-center'>
+              <div className='avatar  chatGpt'>
+              </div>
+              <div className='message'>
+                i am an Ai
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='chat-input-holder'>
+          <textarea className='chat-input-textarea' >
+          </textarea>
+        </div>
+      </section>
     </div>
   );
 }
